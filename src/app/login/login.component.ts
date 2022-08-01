c6116985efbbf07a82fa55f27872306d532ba1d6
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2022-08-01 15:12:33 -0400

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -21,26 +21,19 @@ export class LoginComponent implements OnInit {
   constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {}
-  onSubmit(): void {
-    if (this.user.userName == '') {
-      this.loading = false;
-      this.warning = 'Username must not be empty';
-    } else if (this.user.password == '') {
-      this.loading = false;
-      this.warning = 'Password must not be empty';
-    } else {
-      this.loading = true;
-      this.auth.login(this.user).subscribe(
-        (msg) => {
-          this.loading = false;
-          localStorage.setItem('access_token', msg.token);
-          this.router.navigate(['/newReleases']);
-        },
-        (err) => {
-          this.loading = false;
-          this.warning = err.error.message;
-        }
-      );
+    onsubmit(){
+      if(this.user.userName != "" && this.user.password != ""){
+        this.loading = true;
+        this.auth.login(this.user).subscribe(
+          {next: (data) => {
+            this.loading = false;
+            localStorage.setItem('access_token', data.token);
+            this.router.navigate(['/newRelease']);
+          },error: (err) => {
+            this.loading = false;
+            this.warning = err.error.message;
+          }}
+        )
+      }
     }
   }
-}
