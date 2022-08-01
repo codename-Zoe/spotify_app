874222240fa8e775a58c4f66bf683edebeebc93b
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2022-08-01 12:42:55 -0400

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   clientID: '291d58286dba447b8d5ca4468a9023de',
   clientSecret: 'd31dce44ed4848a29db25db79b35aade',
-  userAPIBase: 'https://whispering-eyrie-06300.herokuapp.com/api/user'
+  userAPIBase: 'hhttps://limitless-tundra-16641.herokuapp.com/api/user'
 };
